19baee4b7236844f16a0d8ef708e3a305585718e
AshliB <ashlibullock16@gmail.com>
2020-07-23 18:25:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+char = input('>> enter a char:')
+num = int(input('>> enter a positive number (>5):'))
+
+for i in range(1, num + 1): 
+  text=''
+  for j in range(i):
+    text+=char
+
+  print(text)
